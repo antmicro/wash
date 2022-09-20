4a40967e96cec278686c390eff1aff3960b51768
Grzegorz Placzek <gplaczek@internships.antmicro.com>
2022-09-20 12:21:07 +0200

    Fix index out of range panic

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -429,7 +429,7 @@ fn handle_simple_word<'a>(shell: &'a Shell, word: &'a ast::DefaultSimpleWord) ->
             }
             ast::Parameter::At => { Some(shell.args[1..].join(" ")) },
             ast::Parameter::Pound => { Some(format!("{}", shell.args.len() - 1)) },
-            ast::Parameter::Positional(n) => { Some(String::from(&shell.args[*n as usize])) }
+            ast::Parameter::Positional(n) => { Some(String::from(if let Some(a) = &shell.args.get(*n as usize) { a } else { "" })) }
             any => Some(format!("parameter not yet handled: {:?}", any)),
         },
         any => Some(format!("simple word not yet handled: {:?}", any)),
